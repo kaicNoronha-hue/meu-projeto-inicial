a75f38497be8d42a46aa9356114bafd6e108e5af
unknown <you@example.comkn.noronha04@gmail.com>
2020-03-25 09:07:01 -0300

    Versão incial do projeto

---
+++ src/test/java/com/Information/ProjetoInicial/api/ProjetoInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Information.ProjetoInicial.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
